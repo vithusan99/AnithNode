715a12ccb08c7edc817f606410c9ea0c337a72a9
Anith Vithushan <vithusan091999@gmail.com>
2023-08-23 18:23:16 +0530

    fetched data displayed

--- public/script.js
+++ public/script.js
@@ -3,6 +3,9 @@ console.log('script works well');
 // calling requests 
 const countryBtn = document.querySelector('#country');
 
+const ul = document.getElementById('countryList');
+const list = document.createDocumentFragment();
+
 countryBtn.addEventListener('click',()=>{
     countryBtn.setAttribute('disabled',true);
     countryBtn.classList.add('disabled')
@@ -16,20 +19,37 @@ async function getThingData(){
     const response = await fetch('/things')
     .then(response => {
         // Handle successful response
-        countryBtn.removeAttribute('disabled');
-        countryBtn.classList.remove('disabled')
+       
         return response.json();
-    })
-    .catch(error => {
-        // Handle error
+    }).then(data=>{
+        let country=data
+
         countryBtn.removeAttribute('disabled')
         countryBtn.classList.remove('disabled')
+        country?.autocomplete.map(item=>{
+            console.log(item);
+            let li = document.createElement('li');
+            let name = document.createElement('h2');
+
+            name.innerHTML = `${item._id}`;
+
+            li.appendChild(name);
+            list.appendChild(li);
+            // document.querySelector('#countryList').innerText=item._id;
+        })
+    }).catch(error => {
+        // Handle error
+        countryBtn.removeAttribute('disabled');
+        countryBtn.classList.remove('disabled');
         console.error('An error occurred:', error);
     });
-    console.log(response);
-    let randomDataCountry= response.autocomplete[Math.floor(Math.random() * response?.autocomplete?.length)];
-    let randomDataCity= response.autocomplete[Math.floor(Math.random() * response?.autocomplete?.length)]?.city
-    console.log(randomDataCountry,randomDataCity);
+
+    ul.appendChild(list);
+    
+    // console.log(response?.autocomplete);
+    // let randomDataCountry= response.autocomplete[Math.floor(Math.random() * response?.autocomplete?.length)];
+    // let randomDataCity= response.autocomplete[Math.floor(Math.random() * response?.autocomplete?.length)]?.city
+    // console.log(randomDataCountry,randomDataCity);
 }
 
 async function getWeatherData(){
